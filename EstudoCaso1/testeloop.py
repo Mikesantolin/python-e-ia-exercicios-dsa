f2a1100cf494e5809281f701b9edcff1a4a45358
Mike Santolin da Silva <mikesantolin@gmail.com>
2026-09-13 21:52:18 -0300

    Ex8
    Cap4 realizado até exercício 8.

---
+++ EstudoCaso1/testeloop.py
@@ -0,0 +1,7 @@
+nota1 = int(input())
+nota2 = int(input())
+media = (nota1 + nota2) /2
+
+if media >= 7 :
+    print(f"Sua nota1 foi {nota1} e nota2 foi {nota2} e sua média foi:{media}, está aprovado")
+else: print(f"Sua nota1 foi {nota1} e nota2 {nota2} e sua média foi:{media}, está reprovado")
